d18f28fdac845293bcea5556c0296784836a8479
Jacob Seppanen <jbseppanen@gmail.com>
2018-11-12 21:50:55 -0800

    Functioning main activity.

--- app/src/main/java/com/example/jacob/android_readinglist/Book.java
+++ app/src/main/java/com/example/jacob/android_readinglist/Book.java
@@ -5,9 +5,9 @@ import java.io.Serializable;
 public class Book implements Serializable {
     public static final int NO_ID = -1;
 
-    private String title, reasonToRead;
-    private boolean hasBeenRead;
-    private int id;
+    public String title, reasonToRead;
+    public boolean hasBeenRead;
+    public int id;
 
     public Book(int id, String title, String reasonToRead, boolean hasBeenRead) {
         this.id = id;
@@ -18,11 +18,50 @@ public class Book implements Serializable {
 
     public Book(String csvString) {
         String[] values = csvString.split(",");
-        this.id = Integer.parseInt(values[1]);
-        this.title = values[0];
-        this.reasonToRead = values[1];
-
+        this.id = Integer.parseInt(values[0]);
+        this.title = values[1];
+        this.reasonToRead = values[3];
+        if (values[4].equals("true")) {
+            this.hasBeenRead = true;
+        } else {
+            this.hasBeenRead = false;
+        }
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getReasonToRead() {
+        return reasonToRead;
+    }
+
+    public void setReasonToRead(String reasonToRead) {
+        this.reasonToRead = reasonToRead;
+    }
+
+    public boolean isHasBeenRead() {
+        return hasBeenRead;
+    }
+
+    public void setHasBeenRead(boolean hasBeenRead) {
+        this.hasBeenRead = hasBeenRead;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String toCSVString() {
+     return this.id + "," + this.title.replaceAll(",","") + "," + this.reasonToRead.replaceAll(",","") + "," + Boolean.toString(this.hasBeenRead);
+    }
 
 }
