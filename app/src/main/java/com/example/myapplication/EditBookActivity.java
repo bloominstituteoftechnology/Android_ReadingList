d2a587ff2b5259ea0b0aa3f0d267e7601f0dd09f
BL-CoderOvertime <brandonlively@live.com>
2019-03-25 16:43:19 -0400

    Set up EditBook layout

--- app/src/main/java/com/example/myapplication/EditBookActivity.java
+++ app/src/main/java/com/example/myapplication/EditBookActivity.java
@@ -2,12 +2,19 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class EditBookActivity extends AppCompatActivity {
+
+    EditText tvReasonsToRead;
+    EditText tvTitle;
+    Switch hasBeenRead;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_book);
     }
+
 }
