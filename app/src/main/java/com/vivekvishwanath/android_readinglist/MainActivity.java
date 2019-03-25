de77c6886903dc58a3e546283d28e221c0dfe9e4
VivekV95 <vivekv210@gmail.com>
2019-03-25 15:30:11 -0500

    Added functionality to Add Book button to navigate to edit view

--- app/src/main/java/com/vivekvishwanath/android_readinglist/MainActivity.java
+++ app/src/main/java/com/vivekvishwanath/android_readinglist/MainActivity.java
@@ -1,18 +1,34 @@
 package com.vivekvishwanath.android_readinglist;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Context context;
+    Button addBookButton;
+    LinearLayout bookViewLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         context = this;
+
+        addBookButton = findViewById(R.id.add_book_button);
+        addBookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, EditBookActivity.class);
+                startActivity(intent);
+            }
+        });
+        bookViewLayout = findViewById(R.id.book_view_layout);
 
     }
 
