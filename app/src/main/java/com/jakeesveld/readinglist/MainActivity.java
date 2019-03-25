1b986b6332fdd9dada27f7c1d677546fa2cb2806
Jake Esveld <thejakeesveld@gmail.com>
2019-03-25 12:33:21 -0700

    Added createBookView method, populated dummy data for testing

--- app/src/main/java/com/jakeesveld/readinglist/MainActivity.java
+++ app/src/main/java/com/jakeesveld/readinglist/MainActivity.java
@@ -1,5 +1,7 @@
 package com.jakeesveld.readinglist;
 
+import android.content.Context;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
+    LinearLayout layoutList;
+    static ArrayList<Book> bookList;
+    static int nextId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,32 +28,69 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        context = this;
+        layoutList = findViewById(R.id.layout_list);
+        bookList = new ArrayList<>();
+        bookList.add(createDummyBook());
+        bookList.add(createDummyBook());
+        bookList.add(createDummyBook());
+        bookList.add(createDummyBook());
+
+        for(Book book: bookList){
+            layoutList.addView(createBookView(book));
+        }
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
             }
         });
     }
 
+    public TextView createBookView(Book addedBook){
+        TextView newBook = new TextView(context);
+
+        newBook.setText(addedBook.getTitle());
+        if(addedBook.getHasBeenRead()){
+            newBook.setPaintFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+        }
+        newBook.setTextSize(22);
+        newBook.setPadding(10,10,10,10);
+
+
+        return newBook;
+    }
+
+
+    public Book createDummyBook(){
+        Book dummyBook = new Book(
+                "Dummy Title",
+                "It's a book for dummies",
+                true,
+                nextId++
+        );
+
+        return dummyBook;
+    }
+
+
+
+
+
+
+
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
