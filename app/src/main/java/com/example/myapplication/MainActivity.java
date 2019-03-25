27bc23454ec52f8b467687221b234e435c9a2892
BL-CoderOvertime <brandonlively@live.com>
2019-03-25 16:12:17 -0400

    Added and fleshed out book object, tested the create text view and cvs conversion functions, all systems go. (i forgot to commit sooner)

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,8 +10,13 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Context context;
+    LinearLayout ll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        context = this;
+        ll = findViewById(R.id.ll_scrollview);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +35,18 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+/*        String bookString1 = "1,The Great Escape1,It's a good book,read";
+        String bookString2 = "1,The Great Escape2,It's a good book,read";
+        String bookString3 = "1,The Great Escape3,It's a good book,read";
+
+        Book b1 = new Book(bookString1);
+        Book b2 = new Book("1","The Great Escape","It's a good book",true);
+        Book b3 = new Book(bookString3);
+
+        ll.addView(buildItemView(b1));
+        ll.addView(buildItemView(b2));
+        ll.addView(buildItemView(b3));*/
     }
 
     @Override
@@ -49,4 +70,21 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public TextView buildItemView(Book book){
+        TextView tv = new TextView(context);
+        tv.setText(book.getTitle());
+        tv.setTextSize(24);
+        tv.setPadding(10,5,10,5);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+            }
+        });
+
+        return tv;
+    }
+
+
 }
