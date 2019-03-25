2c8ab16f34c861ea8905de8fd309192cde0f5f3a
heiligbasil <basilief@yahoo.com>
2019-03-25 15:22:34 -0700

    Clicking the floating button now whisks the user to the Edit Activity

--- app/src/main/java/com/lambdaschool/android_readinglist/MainActivity.java
+++ app/src/main/java/com/lambdaschool/android_readinglist/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lambdaschool.android_readinglist;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -38,8 +39,10 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+/*                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Intent intent = new Intent(context,EditBookActivity.class);
+                startActivity(intent);
             }
         });
     }
