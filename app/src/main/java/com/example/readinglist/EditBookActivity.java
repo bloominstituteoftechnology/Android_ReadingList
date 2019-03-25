a596d78536dd7aacb0695d8969ccb13b113c7a60
Spencer Stock <spencerstock@gmail.com>
2019-03-25 16:43:14 -0600

    Basic functionality for adding books -> views

--- app/src/main/java/com/example/readinglist/EditBookActivity.java
+++ app/src/main/java/com/example/readinglist/EditBookActivity.java
@@ -1,9 +1,12 @@
 package com.example.readinglist;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class EditBookActivity extends AppCompatActivity {
@@ -11,15 +14,19 @@ public class EditBookActivity extends AppCompatActivity {
     String bookCsv;
     EditText name;
     EditText reasonToRead;
+    CheckBox checkBox;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_book);
 
+        context = this;
         Intent intent = getIntent();
         name = findViewById(R.id.view_text_name);
         reasonToRead = findViewById(R.id.view_text_reason_to_read);
+        checkBox = findViewById(R.id.checkbox_already_read);
 
         try {
             id = intent.getStringExtra("id");
@@ -38,6 +45,28 @@ public class EditBookActivity extends AppCompatActivity {
         }
 
 
+
+    }
+
+    public void returnData() {
+
+        Book book = new Book(id,
+                name.getText().toString(),
+                reasonToRead.getText().toString(),
+                checkBox.isChecked());
+        String temp = Book.toCsvString(book);
+
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("BOOK_KEY", temp);
+        setResult(Activity.RESULT_OK, resultIntent);
+        finish();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+      //  super.onBackPressed();
+        returnData();
 
     }
 }
--- app/src/main/java/com/example/readinglist/MainActivity.java
+++ app/src/main/java/com/example/readinglist/MainActivity.java
@@ -1,20 +1,26 @@
 package com.example.readinglist;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     Context context;
     LinearLayout linearLayout;
     Button button;
+    ArrayList<Book> bookList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,13 +28,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         linearLayout = findViewById(R.id.layout_linear_child);
         context = this;
+        bookList = new ArrayList<>();
 
 
         //Add a couple hardcoded books for testing
-        Book book1 = new Book("0,The Martian,I like Martians,1");
-        Book book2 = new Book("1,The Girl on The Train,I like Trains,1");
-        linearLayout.addView(BuildItemView(book1));
-        linearLayout.addView(BuildItemView(book2));
+        bookList.add(new Book("0,The Martian,I like Martians,1"));
+        bookList.add(new Book("1,The Girl on The Train,I like Trains,1"));
+        for (Book book: bookList) {
+            linearLayout.addView(BuildItemView(book));
+        }
 
         button = findViewById(R.id.button_add_book);
         button.setOnClickListener(new View.OnClickListener() {
@@ -38,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
                 //handles the case of pushing the button
                 String id = Integer.toString(linearLayout.getChildCount());
                 intent.putExtra("id", id);
-                startActivity(intent);
+                startActivityForResult(intent, 50);
             }
         });
 
@@ -46,7 +54,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+//        for (Book book: bookList) {
+//            linearLayout.addView(BuildItemView(book));
+//        }
 
+    }
 
     public TextView BuildItemView(final Book book){
         TextView tv = new TextView(context);
@@ -62,9 +77,31 @@ public class MainActivity extends AppCompatActivity {
                 //handles the case of tapping an already existing book
                 String temp = Book.toCsvString(book);
                 intent.putExtra("BookCsv", temp);
-                startActivity(intent);
+                startActivityForResult(intent, 50);
             }
         });
         return tv;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i("LogTest", "onactivityresult ran successfully");
+        if (true) {
+            //TODO: adjust views based on bookList
+        //if (requestCode == 50 && resultCode == Activity.RESULT_OK) {
+            String temp = data.getStringExtra("BOOK_KEY");
+            Book book = new Book(temp);
+            int indexTemp = Integer.parseInt(book.getId());
+            //Will decide whether to add the book to the list, or to change a currently held book
+            if (bookList.size() <= indexTemp){
+                bookList.add(book);
+            } else {
+                bookList.set (indexTemp, book);
+            }
+            linearLayout.addView(BuildItemView(book));
+
+        }
     }
 }
