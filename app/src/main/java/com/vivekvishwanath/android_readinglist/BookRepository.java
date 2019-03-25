5811331591e2ba3bf18c7da0582ab9c865fbe086
VivekV95 <vivekv210@gmail.com>
2019-03-25 17:17:05 -0500

    Added a Book repository class

---
+++ app/src/main/java/com/vivekvishwanath/android_readinglist/BookRepository.java
@@ -0,0 +1,19 @@
+package com.vivekvishwanath.android_readinglist;
+
+import java.util.ArrayList;
+
+public class BookRepository {
+    private static ArrayList<Book> bookList = new ArrayList<>();
+
+    public static void addBook(Book book) {
+        bookList.add(book);
+    }
+
+    public static Book getBook(int index) {
+        return bookList.get(index);
+    }
+
+    public static ArrayList<Book> getBookList() {
+        return bookList;
+    }
+}
--- app/src/main/java/com/vivekvishwanath/android_readinglist/MainActivity.java
+++ app/src/main/java/com/vivekvishwanath/android_readinglist/MainActivity.java
@@ -2,6 +2,7 @@ package com.vivekvishwanath.android_readinglist;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     Context context;
     Button addBookButton;
     LinearLayout bookViewLayout;
+    public static final int BOOK_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(context, EditBookActivity.class);
                 intent.putExtra(Book.NEW_BOOK_TAG, bookViewLayout.getChildCount());
-                startActivity(intent);
+                startActivityForResult(intent, BOOK_REQUEST_CODE);
             }
         });
         bookViewLayout = findViewById(R.id.book_view_layout);
@@ -42,9 +44,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.putExtra(Book.EDIT_BOOK_TAG, book.toCsvString());
-                startActivity(intent);
+                startActivityForResult(intent, BOOK_REQUEST_CODE);
             }
         });
         return bookView;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == BOOK_REQUEST_CODE && requestCode == RESULT_OK) {
+            String bookCsv = data.getStringExtra(Book.EDIT_BOOK_TAG);
+        }
     }
 }
