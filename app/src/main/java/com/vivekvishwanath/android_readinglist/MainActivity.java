f56159910868fc7e31c29ef82c594e383d7b97ab
VivekV95 <vivekv210@gmail.com>
2019-03-25 15:12:02 -0500

    Added TextView generator method for main layout to display books

--- app/src/main/java/com/vivekvishwanath/android_readinglist/MainActivity.java
+++ app/src/main/java/com/vivekvishwanath/android_readinglist/MainActivity.java
@@ -1,13 +1,24 @@
 package com.vivekvishwanath.android_readinglist;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        context = this;
+    }
+
+    private TextView buildItemView(Book book) {
+        TextView bookView = new TextView(context);
+        bookView.setText(book.getTitle());
+        bookView.setTextSize(18);
+        return bookView;
     }
 }
