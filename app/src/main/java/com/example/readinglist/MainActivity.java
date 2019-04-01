dc2347acf86766a7127789ec162622f487d343dd
voodoopdx <ourquotesdefineus@gmail.com>
2019-04-01 14:32:30 -0700

    Activities Linked

--- app/src/main/java/com/example/readinglist/MainActivity.java
+++ app/src/main/java/com/example/readinglist/MainActivity.java
@@ -6,19 +6,33 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     Context context;
+    Button addBookButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        context = this;
 
+        LinearLayout lineMan = (LinearLayout) findViewById(R.id.line_man);
+
+        Button addBttn = findViewById(R.id.add_book);
+        addBttn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(context, EditBookActivity.class);
+                startActivityForResult(i, 0);
+            }
+        });
     }
 
     private TextView buildItemView(final Book b) {
@@ -28,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         textView.setText(String.format("Title: %t ", b.getTitle()));
         return textView;
     }
+
+
 
 
 }
