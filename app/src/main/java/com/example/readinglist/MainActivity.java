9895e74dfbc6fc4fe1283c505b98ac847541272d
voodoopdx <ourquotesdefineus@gmail.com>
2019-04-01 13:24:57 -0700

    MainActivity Complete

--- app/src/main/java/com/example/readinglist/MainActivity.java
+++ app/src/main/java/com/example/readinglist/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.readinglist;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+    private TextView buildItemView(final Book b) {
+        TextView textView = new TextView(context);
+        textView.setTextSize(25);
+        textView.setTextColor(Color.BLACK);
+        textView.setText(String.format("Title: %t ", b.getTitle()));
+        return textView;
+    }
+
+
 }
